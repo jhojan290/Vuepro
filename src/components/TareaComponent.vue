66e6e90b7a390ace19ac59c45fbb41c70134284d
jhojan290 <davio13bar@gmail.com>
2024-02-06 18:55:49 -0500

    nuevo-commit

---
+++ src/components/TareaComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1 class="display-4 text-center">Listado de Tareas</h1>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'TareaComponent'
+    }
+
+</script>
